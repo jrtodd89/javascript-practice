338c6101b7c3a1a5ea673dced13362157c7118c7
Voldar <48755833+Voldar@users.noreply.github.com>
2022-08-10 21:10:51 -0700

    Day 2 daily challenge

---
+++ js/main.js
@@ -0,0 +1,7 @@
+console.log('Supercalifragilisticexpialidocious'.length);
+
+console.log(14)
+console.log(typeof 14)
+
+console.log(578489694856847373765896934856768949486576748385n)
+console.log(typeof 578489694856847373765896934856768949486576748385n)
