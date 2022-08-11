0a0da2217066ca7b2a013519c87e7c4c6f0e1e54
Voldar <48755833+Voldar@users.noreply.github.com>
2022-08-10 21:20:26 -0700

    Day 2 daily challenge optimizations

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 console.log('Supercalifragilisticexpialidocious'.length);
 
-console.log(14)
-console.log(typeof 14)
+let num = 14;
+console.log(num);
+console.log(typeof num);
 
-console.log(578489694856847373765896934856768949486576748385n)
-console.log(typeof 578489694856847373765896934856768949486576748385n)
+let bigInt = 578489694856847373765896934856768949486576748385n;
+console.log(bigInt);
+console.log(typeof bigInt);
