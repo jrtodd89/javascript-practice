c1968d5f3497877298b6bd7515811c7f64de4bd2
Voldar <48755833+Voldar@users.noreply.github.com>
2022-08-22 22:46:50 -0700

    Take home work

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@
 //Alert the user whether the given number is even or odd
 //Call your function
 
-// function numberAlert(input) {
-// 	if (input % 2 == 0) {
-// 		alert(`Your number is even!`)
-// 	} else if (input % 2 != 0) {
-// 		alert(`Your number is odd!`)
-// 	}
-// }
-// numberAlert(49859820);
+function numberAlert(input) {
+	if (input % 2 == 0) {
+		alert(`Your number is even!`)
+	} else if (input % 2 != 0) {
+		alert(`Your number is odd!`)
+	}
+}
+// numberAlert(42);
 
 // Write a function that accepts a password (string)
 // Validate the password using the following rules:
@@ -19,44 +19,47 @@
 // Alert the user if their password has been accepted or why their password was rejected
 
 function password(input) {
+	let regExp = /[a-z]/i;
 	if (input.length < 6) {
 		alert(`Password is too short!`)
 	} else if (input.length > 20) {
 		alert(`Password is too long!`)
-	} else if (input[0] ){
-		console.log(input.length);
+	} else if (regExp.test(input.charAt(0))) {
+		alert(`Password has been accepted.`)
+	} else {
+		alert(`Password does not begin with a letter.`)
 	}
 }
 
-password('deKmN30ofe6OdeKmN30ofe6O');
+password('deKmN00ofe6O');
 
 
 
-function createElement(type) {
-	document.createElement(type);
-}
+// function createElement(type) {
+// 	document.createElement(type);
+// }
 
-function placeElement(element, parent) {
-	parent.appendChild(element);
-}
+// function placeElement(element, parent) {
+// 	parent.appendChild(element);
+// }
 
-function updateElement(element, html) {
-	element.innerHTML = html;
-}
+// function updateElement(element, html) {
+// 	element.innerHTML = html;
+// }
 
-function updateElementStyle(element, styleName, styleValue) {
-	element.style[styleName] = styleValue;
-}
+// function updateElementStyle(element, styleName, styleValue) {
+// 	element.style[styleName] = styleValue;
+// }
 
-function updateElementClass(element, className) {
-	element.classList.add(className);
-}
+// function updateElementClass(element, className) {
+// 	element.classList.add(className);
+// }
 
-function removeElementClass(element, className) {
-	element.classList.remove(className);
-}
+// function removeElementClass(element, className) {
+// 	element.classList.remove(className);
+// }
 
-function removeElement(element){
-	element.remove();
-}
+// function removeElement(element){
+// 	element.remove();
+// }
 
