3c143b8965dfaa871fa32033fb8b7af721c050ee
Voldar <48755833+Voldar@users.noreply.github.com>
2022-08-22 22:53:55 -0700

    updates

--- js/main.js
+++ js/main.js
@@ -54,7 +54,7 @@
 // ------------ TAKE HOME CHALLENGE ------------
 // Add an edge case to your daily challenge code
 // Check if the user input anything other than a number
-// If so, prompt the user again to input a number
+// If so, prompt the user again to input a number 
 
 let numberOne = '';
 let numberTwo = '';
